62dbf8e17266801b6c986f170b25b2903ef20daf
yfq <304597590@qq.com>
2022-12-28 17:05:11 +0800

    了解重要的基本概念以及一维数组

--- 练习/helloworld.c
+++ 练习/helloworld.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-    
+
+// 声明一维数组
+int array1[]={ };
+// 初始化数组
+int array2[]={1,2,3,4};
+
+
 int main()
 {
-    printf("beatu");
+    int index;
+// 访问数组元素
+    scanf("%d",&index);
+    printf("array %d is %d",index,array2[index]);
     return 0;
 }
